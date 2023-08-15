d03d8d2589c309d55ac62602455b221c9bd9a5d6
zlyons70 <zlyons70@gmail.com>
2023-08-14 23:18:47 -0400

    Set up flask and react

--- tip-tracker/src/App.js
+++ tip-tracker/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  // data is the variable setData manipulates the data variable
+  const [data, setData] = useState([{}])
+// useEffect uses /members route, and then it sets the data variable to the data it gets from the route
+  useEffect(() => {
+    fetch("/members").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      {(typeof data.members === 'undefined') ? (
+        <p>loading...</p>
+      ): (
+          data.members.map((member, index) => (
+            <p key={index}>{member}</p>
+          ))
+      )}
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
